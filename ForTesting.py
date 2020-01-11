1bada12b850206f7a8edbf810eb31408d89d9a2c
GNiramay <niramaygogate@gmail.com>
2020-01-11 17:47:17 +0530

    ML using Keras

---
+++ ForTesting.py
@@ -0,0 +1,67 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import LoadData_Nocut as ld
+import ROOT
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from ROOT import TFile,TTree
+import array
+import sys
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # to supress unwanted errors
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+def ML_Regression(ENR):
+    DATE = "aaaa_16_12_19"
+    if len(ENR)==1:
+        EName = "%s_E%s" % (DATE,ENR[0])
+    else:
+        EName = "%s_E_All" % DATE
+    [xtrain,ytrain,xtest,ytest] = ld.GetData(0.75,ENR,'QGSP_FTFP_BERT_EML')
+    [ntrain,nnode] = np.shape(xtrain)
+
+    ############ ML model setup
+    model = Sequential()
+    # model.add(Dense(nnode, input_dim=nnode, activation='linear',use_bias=False))
+    # model.add(Dense(1, input_dim=nnode, activation='linear',use_bias=False))
+    model.add(Dense(nnode, input_dim=nnode, activation='linear'))
+    model.add(Dense(1, input_dim=nnode, activation='linear'))
+    model.summary()
+    
+    ############ ML Training
+    CFNAME = "mse"                  # cost function-> min. standard error
+    model.compile(optimizer='adam', loss='mse', metrics=['mse'])
+    model.fit(xtrain,ytrain, batch_size=100, epochs=20, verbose=0, shuffle=True)
+    model.save("ForTesting.h5")
+
+def LaterUse(ENR):
+    import numpy as np
+    import tensorflow as tf
+    from keras.models import load_model
+    model = load_model('ForTesting.h5')
+    model.summary()
+    [xtrain,ytrain,xtest,ytest] = ld.GetData(0.1,ENR,'QGSP_FTFP_BERT_EML')
+
+    predict = model.predict(xtest)
+    EvalOut = TFile("ForTesting.root","RECREATE")
+    ttr  = TTree("MainTree","to check performance of ML training")
+
+    BeamEn = np.array(ytest[0])
+    RecoEn = np.array(predict[0])
+        
+    Y1 = ttr.Branch("BeamEn",BeamEn,"BeamEn/F")
+    Y2 = ttr.Branch("RecoEn",RecoEn,"RecoEn/F")
+        
+    for ii in range(len(ytest)):
+        BeamEn[0] = ytest[ii]
+        RecoEn[0] = predict[ii]
+        ttr.Fill()
+    ttr.Write()
+    EvalOut.Close()
+
+
+ML_Regression([100])
+LaterUse([100])
